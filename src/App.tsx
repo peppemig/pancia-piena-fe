28f8cde5a5fb5df0d69c29e196f7570a80234973
giuseppe.migliozzi@outlook.it <giuseppe.migliozzi@outlook.it>
2023-11-01 12:17:02 +0100

    added order functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,42 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Navbar from "./components/navbar/Navbar";
 import Register from "./pages/Register";
-import { ThemeProvider } from "./components/ThemeProvider";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./config/firebaseConfig";
 import { Toaster } from "@/components/ui/toaster";
-//import LoadingState from "./components/LoadingState";
+import LoadingState from "./components/LoadingState";
 import Products from "./pages/Products";
 import Orders from "./pages/Orders";
 import Dashboard from "./pages/Dashboard";
 import CreateOrder from "./pages/CreateOrder";
 
 function App() {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   //console.log("LOADING: " + loading);
   //console.log("USER: " + JSON.stringify(user));
 
-  //if (loading) {
-  //  return <LoadingState />;
-  //}
+  if (loading) {
+    return <LoadingState />;
+  }
 
   return (
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <Toaster />
-        <Navbar user={user} />
-        <Routes>
-          <Route path="/" element={<Home user={user} />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="products">
-            <Route path="list" element={<Products user={user} />} />
-          </Route>
-          <Route path="orders">
-            <Route path="list" element={<Orders user={user} />} />
-            <Route path="create" element={<CreateOrder user={user} />} />
-          </Route>
-          <Route path="dashboard" element={<Dashboard user={user} />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Toaster />
+      <Navbar user={user} />
+      <Routes>
+        <Route path="/" element={<Home user={user} />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="products">
+          <Route path="list" element={<Products user={user} />} />
+        </Route>
+        <Route path="orders">
+          <Route path="list" element={<Orders user={user} />} />
+          <Route path="create" element={<CreateOrder user={user} />} />
+        </Route>
+        <Route path="dashboard" element={<Dashboard user={user} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/CreateOrder.tsx
+++ src/pages/CreateOrder.tsx
@@ -20,8 +20,10 @@ type Product = {
 };
 
 type OrderItem = {
-  quantity: number;
   productId: string;
+  quantity: number;
+  productName: string;
+  productPrice: number;
 };
 
 const CreateOrder = ({ user }: CreateOrderProps) => {
@@ -85,7 +87,12 @@ const CreateOrder = ({ user }: CreateOrderProps) => {
     } else {
       setOrderItems((prev) => [
         ...prev,
-        { quantity: 1, productId: product.id },
+        {
+          productId: product.id,
+          quantity: 1,
+          productName: product.name,
+          productPrice: product.price,
+        },
       ]);
     }
   };
@@ -113,39 +120,76 @@ const CreateOrder = ({ user }: CreateOrderProps) => {
     <div className="container-custom py-6 space-y-4">
       <h2 className="text-3xl font-bold tracking-tight">Crea un ordine</h2>
       {products.length > 0 && (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-0 lg:gap-4">
           <div className="col-span-2 p-6 space-y-4 rounded-lg border bg-card text-card-foreground shadow-sm w-full">
-            {products.map((product) => (
-              <div
-                key={product.id}
-                className="flex items-center justify-between"
-              >
-                <div>
-                  <p className="text-xl font-bold">{product.name}</p>
-                  <p className="text-lg font-medium">€{product.price}</p>
+            {products.map((product, index) => (
+              <div key={product.id}>
+                <div className="flex items-center justify-between">
+                  <div>
+                    <p className="text-xl font-bold">{product.name}</p>
+                    <p className="text-lg font-medium">€{product.price}</p>
+                  </div>
+                  <div className="flex items-center justify-center gap-4">
+                    <Button
+                      disabled={
+                        !orderItems.some(
+                          (item) => item.productId === product.id
+                        )
+                      }
+                      variant="ghost"
+                      onClick={() => removeQuantity(product)}
+                    >
+                      <MinusCircle size={30} />
+                    </Button>
+                    <p className="text-lg font-medium">
+                      {orderItems.find((item) => item.productId === product.id)
+                        ?.quantity ?? 0}
+                    </p>
+                    <Button
+                      variant="ghost"
+                      onClick={() => addQuantity(product)}
+                    >
+                      <PlusCircle size={30} />
+                    </Button>
+                  </div>
                 </div>
-                <div className="flex items-center justify-center gap-4">
-                  <Button
-                    disabled={
-                      !orderItems.some((item) => item.productId === product.id)
-                    }
-                    variant="ghost"
-                    onClick={() => removeQuantity(product)}
-                  >
-                    <MinusCircle size={30} />
-                  </Button>
-                  <p className="text-lg font-medium">
-                    {orderItems.find((item) => item.productId === product.id)
-                      ?.quantity ?? 0}
-                  </p>
-                  <Button variant="ghost" onClick={() => addQuantity(product)}>
-                    <PlusCircle size={30} />
-                  </Button>
-                </div>
+                {index !== products.length - 1 && (
+                  <Separator className="mt-4" />
+                )}
               </div>
             ))}
           </div>
-          <div>TEST</div>
+          <div className="sticky mt-2 lg:mt-0 top-6 h-fit col-span-1 p-6 space-y-4 rounded-lg border bg-card text-card-foreground shadow-sm w-full">
+            <p className="text-xl font-bold">Riepilogo ordine</p>
+            {orderItems.map((item) => (
+              <div
+                key={item.productId}
+                className="flex items-center justify-between"
+              >
+                <div className="flex flex-col">
+                  <p className="text-lg font-medium">{item.productName}</p>
+                  <p className="text-md">Quantità: {item.quantity}</p>
+                </div>
+                <p className="text-lg font-bold">
+                  €{item.quantity * item.productPrice}
+                </p>
+              </div>
+            ))}
+            <Separator />
+            <div className="flex items-center justify-between text-xl font-bold">
+              <p>Totale:</p>
+              <p>
+                €
+                {orderItems.reduce(
+                  (total, item) => total + item.quantity * item.productPrice,
+                  0
+                )}
+              </p>
+            </div>
+            <Button disabled={orderItems.length === 0} className="w-full">
+              Effettua ordine
+            </Button>
+          </div>
         </div>
       )}
     </div>
