5f34690ca5d88c478c68522a472e4660478a30e6
giuseppe.migliozzi@outlook.it <giuseppe.migliozzi@outlook.it>
2023-11-21 00:12:30 +0100

    api fix

--- src/api/productsService.ts
+++ src/api/productsService.ts
@@ -4,21 +4,27 @@ import { API_BASE_URL } from "@/constants/constants";
 
 const getProducts = async (token: string) => {
   const response = await axios.get(`${API_BASE_URL}/products`, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
   return response;
 };
 
 const createProduct = async (token: string, product: ProductRequest) => {
   const response = await axios.post(`${API_BASE_URL}/products`, product, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
   return response;
 };
 
 const deleteProduct = async (token: string, productId: string) => {
   const response = await axios.delete(`${API_BASE_URL}/products/${productId}`, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
   return response;
 };
@@ -31,7 +37,11 @@ const editProduct = async (
   const response = await axios.put(
     `${API_BASE_URL}/products/${productId}`,
     product,
-    { headers: { Authorization: `Bearer ${token}` } }
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
   );
   return response;
 };
